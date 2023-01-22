7b6fd9f0d98ccadf8186d049547b07e491c93928
Shchepin Daniel <shchepin.man@gmail.com>
2023-01-22 14:10:25 +0200

    Basic

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    const sounds = document.querySelectorAll('.sound-picker button');
+
+    const timeDisplay = document.querySelector('.time-display');
+    
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+}
+
+app();
